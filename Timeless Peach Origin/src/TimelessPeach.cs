31664fccb90f4e4589a000c6efbcb67a10f176a5
Christian Sandford <christian.sandford25@gmail.com>
2019-06-06 12:47:46 -0500

    base push

---
+++ Timeless Peach Origin/src/TimelessPeach.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using SadConsole;
+using Console = SadConsole.Console;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Timeless_Peach_Origin.src.consoles;
+
+namespace Timeless_Peach_Origin {
+
+    class TimelessPeach {
+
+        public const int Width = 120;
+        public const int Height = 43;
+
+        static void Main(string[] args) {
+            // Setup the engine and create the main window.
+            SadConsole.Game.Create(Width, Height);
+
+            // Hook the start event so we can add consoles to the system.
+            SadConsole.Game.OnInitialize = Init;
+            SadConsole.Game.OnUpdate = Update;
+
+            // Start the game.
+            SadConsole.Game.Instance.Run();
+            SadConsole.Game.Instance.Dispose();
+        }
+
+        //update function
+        private static void Update(GameTime t) {
+
+        }
+
+        private static void Init() {
+
+            // Begins the game 
+            ContainerConsole game = new ConsoleManager();
+
+        }
+    }
+}
---
+++ Timeless Peach Origin/src/consoles/ConsoleManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SadConsole;
+using Console = SadConsole.Console;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Timeless_Peach_Origin.src.consoles;
+
+namespace Timeless_Peach_Origin.src.consoles {
+    class ConsoleManager : ContainerConsole {
+    }
+}
