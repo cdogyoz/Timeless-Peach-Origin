d0f992439032af6b9c3153e3fee1d5a3bcdf46b2
Christian Sandford <christian.sandford25@gmail.com>
2019-06-06 13:07:35 -0500

    console manager werk

--- Timeless Peach Origin/src/consoles/ConsoleManager.cs
+++ Timeless Peach Origin/src/consoles/ConsoleManager.cs
@@ -6,6 +6,35 @@ using Microsoft.Xna.Framework.Graphics;
 using Timeless_Peach_Origin.src.consoles;
 
 namespace Timeless_Peach_Origin.src.consoles {
+
+    //Stores all of the info about consoles and controls the game state
+
     class ConsoleManager : ContainerConsole {
+
+        public static MainMenuConsole mainMenu;
+        public static GameConsole game;
+
+        private int gameWidth, gameHeight;
+        private GameState gameState = new GameState();
+
+        public ConsoleManager(int gameWidth, int gameHeight) {
+            mainMenu = new MainMenuConsole();
+            gameState = GameState.MAIN_MENU;
+        }
+
+        public override void Update(TimeSpan timeElapsed) {
+
+            base.Update(timeElapsed);
+        }
+
+        public void ChangeState(GameState gameState) {
+            this.gameState = gameState;
+
+            if (gameState == GameState.MAIN_MENU) {
+                SadConsole.Global.CurrentScreen = mainMenu;
+            }
+
+        }
+
     }
 }
