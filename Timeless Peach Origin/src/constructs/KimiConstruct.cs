11e8789841c66b2b40e96406d6821463e537a696
Christian Sandford <christian.sandford25@gmail.com>
2019-06-07 11:52:22 -0500

    changed kimi color

--- Timeless Peach Origin/src/constructs/KimiConstruct.cs
+++ Timeless Peach Origin/src/constructs/KimiConstruct.cs
@@ -6,7 +6,7 @@ using Microsoft.Xna.Framework;
 namespace Timeless_Peach_Origin.src.constructs {
     class KimiConstruct : PlayableConstruct {
 
-        public KimiConstruct() : base(Color.Green, Color.Black, (int)'@', "Kimi Player") {
+        public KimiConstruct() : base(Color.SandyBrown, Color.Black, (int)'@', "Kimi Player") {
             IsVisible = true;
         }
 
