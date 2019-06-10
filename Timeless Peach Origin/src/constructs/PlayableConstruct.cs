f4fc59219d514747407ffe42b345686797451a94
JD Pappas <jaydeepappas@gmail.com>
2019-06-10 10:42:53 -0500

    QOL change

--- Timeless Peach Origin/src/constructs/PlayableConstruct.cs
+++ Timeless Peach Origin/src/constructs/PlayableConstruct.cs
@@ -5,11 +5,12 @@ using Microsoft.Xna.Framework;
 
 namespace Timeless_Peach_Origin.src.constructs {
     class PlayableConstruct : Construct{
+        public int xPos = 10;
+        public int yPos = 10;
 
         public PlayableConstruct(Color foreground, Color background, int glyph, string name) : base(foreground, background, glyph, name) {
             IsVisible = true;
-            Position = new Point(10, 10);
+            Position = new Point(xPos, yPos);
         }
-
     }
 }
