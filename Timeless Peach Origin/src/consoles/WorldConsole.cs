94d3678b65f9d5f58825d48ba474246593ed425f
Christian Sandford <christian.sandford25@gmail.com>
2019-06-12 13:08:51 -0500

    scrolling fixed

--- Timeless Peach Origin/src/consoles/WorldConsole.cs
+++ Timeless Peach Origin/src/consoles/WorldConsole.cs
@@ -14,7 +14,7 @@ namespace Timeless_Peach_Origin.src.consoles {
         private GameConsole game;
         Dungeon dungeon;
 
-        public WorldConsole(PlayableConstruct player, GameConsole game) : base(110, 32, SadConsole.Global.FontDefault.Master.GetFont(Font.FontSizes.Two), new Rectangle(0,0,50, 50)) {
+        public WorldConsole(PlayableConstruct player, GameConsole game) : base(110, 32, SadConsole.Global.FontDefault.Master.GetFont(Font.FontSizes.Two), new Rectangle(0,0,110, 16)) {
             Position = new Point(0, 0);
             this.player = player;
             this.game = game;
@@ -25,7 +25,7 @@ namespace Timeless_Peach_Origin.src.consoles {
         }
 
         public void CenterOnPlayer(){
-            this.CenterViewPortOnPoint(player.Position);
+            this.CenterViewPortOnPoint(new Point(player.Position.X, player.Position.Y));
         }
 
         public override void Update(TimeSpan timeElapsed) {
